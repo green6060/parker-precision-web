4e70766f0f3548ebdda483976ddc01b48f17887d
Dallas Parker <dallasjamesparker@gmail.com>
2025-07-17 19:24:32 -0600

    Updated About Page Text

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,13 +7,9 @@ export default function About() {
       <main className="max-w-4xl mx-auto px-4 py-16">
         <h1 className="text-3xl font-bold mb-6">About Me</h1>
         <p className="mb-4">
-          Hello! I&apos;m Parker, a professional web contractor specializing in
-          building small, custom websites tailored to your needs.
-        </p>
-        <p>
-          With years of experience in web development, I focus on delivering
-          high-quality, responsive, and user-friendly websites that help your
-          business grow.
+          Hello! I&apos;m Dallas. I&apos;ve been into Web Development since
+          about 2017, and I&apos;m only JUST now getting around to create a
+          portfolio of my work!
         </p>
       </main>
     </div>
